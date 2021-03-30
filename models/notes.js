9214508c8863b8876c802d37ca5bbc7fc23dcf4b
Omar Yakoubi <yakoubiomar2001@gmail.com>
2021-03-30 14:35:35 +0100

    note model and services

---
+++ models/notes.js
@@ -0,0 +1,11 @@
+const { DataTypes } = require('sequelize');
+const db = require("../config/db.config")
+
+const Note = db.define('Note', {
+  note: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+});
+
+Note.sync({ force: true })
